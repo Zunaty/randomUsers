5bb087f885362bef59e216b6e4e839b1846abc8c
Victor Perez <zartora@Victors-MacBook-Pro.local>
2022-03-30 19:56:41 -0600

    getting data from api, get route setup for data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const request = require('request');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get("/api/users", (req, res) => {
+    request(
+        {
+            method: 'GET',
+            uri: 'https://randomuser.me/api/?results=10'
+        },
+        function (error, response, body) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            const data = response.body;
+            const apiData = JSON.parse(data);
+            console.log(apiData);
+
+            if (response.statusCode == 200) {
+                console.log('success');
+            } else {
+                console.log('error with api call');
+            }
+        }
+    ).pipe(res);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server active on: ${PORT}`)
+    console.log(`Server active on: ${PORT}`);
 });
